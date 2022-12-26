054324b6e40f52b91aa9f789b9a150809213411b
Lucas Woibau <lucaswoibau7@gmail.com>
2022-12-26 20:46:06 -0300

    Exe02

---
+++ EXERCICIOS/SECAO_04 - CLASSES-METODOS-MEMBROS ESTATICOS/EXE_S04E02/EXE02/EXE02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace EXE02
+{
+    //Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double media;
+
+            Funcionario f1 = new Funcionario();
+            Funcionario f2= new Funcionario();
+
+            Console.WriteLine("Digite o nome do primeiro funcionário");
+            f1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o salario do primeiro funcionario");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o nome do segundo funcionário");
+            f2.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o salario do segundo funcionario");
+            f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = (f1.Salario + f2.Salario) / 2;
+
+            Console.WriteLine($"A média dos salarios dos funcionarios é {media.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
