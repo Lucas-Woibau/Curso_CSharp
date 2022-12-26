99488261e20d5a084eea37717d4ee461aa9df5e4
Lucas Woibau <lucaswoibau7@gmail.com>
2022-12-26 20:26:56 -0300

    Exe01

--- EXERCICIOS/SECAO_04 - CLASSES-METODOS-MEMBROS ESTATICOS/EXE_S04E01/EXE01/EXE01/Program.cs
+++ EXERCICIOS/SECAO_04 - CLASSES-METODOS-MEMBROS ESTATICOS/EXE_S04E01/EXE01/EXE01/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace EXE01
 {
+    //Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Digite o nome da primeira pessoa: ");
+            p1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a idade da primeira pessoa: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o nome da segunda pessoa: ");
+            p2.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a idade da segunda pessoa: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if(p1.Idade > p2.Idade)
+            {
+                Console.WriteLine($"{p1.Nome} é a pessoa mais velha!");
+            }
+            else
+            {
+                Console.WriteLine($"{p2.Nome} é a pessoa mais velha!");
+            }
         }
     }
 }
