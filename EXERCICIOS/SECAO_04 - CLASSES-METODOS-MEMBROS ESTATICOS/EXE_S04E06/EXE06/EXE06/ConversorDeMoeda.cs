1fef30f05424b0accbf45037f8df7823a500b9c0
Lucas Woibau <lucaswoibau@hotmail.com>
2022-12-27 11:37:56 -0300

    EXE06

---
+++ EXERCICIOS/SECAO_04 - CLASSES-METODOS-MEMBROS ESTATICOS/EXE_S04E06/EXE06/EXE06/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE06
+{
+    internal class ConversorDeMoeda
+    {
+        public static double IOF = 6.0;
+
+        public static double DolarParaReal(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * IOF / 100.0;
+        }
+    }
+}
---
+++ EXERCICIOS/SECAO_04 - CLASSES-METODOS-MEMBROS ESTATICOS/EXE_S04E06/EXE06/EXE06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace EXE06
+{
+
+    /* Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprador por uma pessoa em reais.
+       Informar quantos reais a pessoa vai pagar em dólares, considerando ainda que a pessoa terá que pagar 6% de IOF sobre 
+       o valor em dólar. Criar uma classe ConversorDeMoeda para ser responsável pelos cálculos. */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cotação do dólar:");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dólar a ser comprado: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+
+            Console.WriteLine($"Valor a ser pago em reais: {result.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
